d09558775d36c63f52b0e20d9949e50d180d6b45
manzor99 <manzor99@gmail.com>
2019-07-04 11:30:16 -0500

    Renaming files

--- modmain.lua
+++ modmain.lua
@@ -1,66 +1,61 @@
 PrefabFiles = {
-	"esctemplate",
-	"esctemplate_none",
+	"gnar",
+	"gnar_none",
 }
 
 Assets = {
-    Asset( "IMAGE", "images/saveslot_portraits/esctemplate.tex" ),
-    Asset( "ATLAS", "images/saveslot_portraits/esctemplate.xml" ),
+    Asset( "IMAGE", "images/saveslot_portraits/gnar.tex" ),
+    Asset( "ATLAS", "images/saveslot_portraits/gnar.xml" ),
 
-    Asset( "IMAGE", "images/selectscreen_portraits/esctemplate.tex" ),
-    Asset( "ATLAS", "images/selectscreen_portraits/esctemplate.xml" ),
+    Asset( "IMAGE", "images/selectscreen_portraits/gnar.tex" ),
+    Asset( "ATLAS", "images/selectscreen_portraits/gnar.xml" ),
 	
-    Asset( "IMAGE", "images/selectscreen_portraits/esctemplate_silho.tex" ),
-    Asset( "ATLAS", "images/selectscreen_portraits/esctemplate_silho.xml" ),
+    Asset( "IMAGE", "images/selectscreen_portraits/gnar_silho.tex" ),
+    Asset( "ATLAS", "images/selectscreen_portraits/gnar_silho.xml" ),
 
-    Asset( "IMAGE", "bigportraits/esctemplate.tex" ),
-    Asset( "ATLAS", "bigportraits/esctemplate.xml" ),
+    Asset( "IMAGE", "bigportraits/gnar.tex" ),
+    Asset( "ATLAS", "bigportraits/gnar.xml" ),
 	
-	Asset( "IMAGE", "images/map_icons/esctemplate.tex" ),
-	Asset( "ATLAS", "images/map_icons/esctemplate.xml" ),
+	Asset( "IMAGE", "images/map_icons/gnar.tex" ),
+	Asset( "ATLAS", "images/map_icons/gnar.xml" ),
 	
-	Asset( "IMAGE", "images/avatars/avatar_esctemplate.tex" ),
-    Asset( "ATLAS", "images/avatars/avatar_esctemplate.xml" ),
+	Asset( "IMAGE", "images/avatars/avatar_gnar.tex" ),
+    Asset( "ATLAS", "images/avatars/avatar_gnar.xml" ),
 	
-	Asset( "IMAGE", "images/avatars/avatar_ghost_esctemplate.tex" ),
-    Asset( "ATLAS", "images/avatars/avatar_ghost_esctemplate.xml" ),
+	Asset( "IMAGE", "images/avatars/avatar_ghost_gnar.tex" ),
+    Asset( "ATLAS", "images/avatars/avatar_ghost_gnar.xml" ),
 	
-	Asset( "IMAGE", "images/avatars/self_inspect_esctemplate.tex" ),
-    Asset( "ATLAS", "images/avatars/self_inspect_esctemplate.xml" ),
+	Asset( "IMAGE", "images/avatars/self_inspect_gnar.tex" ),
+    Asset( "ATLAS", "images/avatars/self_inspect_gnar.xml" ),
 	
-	Asset( "IMAGE", "images/names_esctemplate.tex" ),
-    Asset( "ATLAS", "images/names_esctemplate.xml" ),
+	Asset( "IMAGE", "images/names_gnar.tex" ),
+    Asset( "ATLAS", "images/names_gnar.xml" ),
 	
-	Asset( "IMAGE", "images/names_gold_esctemplate.tex" ),
-    Asset( "ATLAS", "images/names_gold_esctemplate.xml" ),
+	Asset( "IMAGE", "images/names_gold_gnar.tex" ),
+    Asset( "ATLAS", "images/names_gold_gnar.xml" ),
 	
-    Asset( "IMAGE", "bigportraits/esctemplate_none.tex" ),
-    Asset( "ATLAS", "bigportraits/esctemplate_none.xml" ),
+    Asset( "IMAGE", "bigportraits/gnar_none.tex" ),
+    Asset( "ATLAS", "bigportraits/gnar_none.xml" ),
 
 }
 
-AddMinimapAtlas("images/map_icons/esctemplate.xml")
+AddMinimapAtlas("images/map_icons/gnar.xml")
 
 local require = GLOBAL.require
 local STRINGS = GLOBAL.STRINGS
 
 -- The character select screen lines
-STRINGS.CHARACTER_TITLES.esctemplate = "The Sample Character"
-STRINGS.CHARACTER_NAMES.esctemplate = "Esc"
-STRINGS.CHARACTER_DESCRIPTIONS.esctemplate = "*Perk 1\n*Perk 2\n*Perk 3"
-STRINGS.CHARACTER_QUOTES.esctemplate = "\"Quote\""
+STRINGS.CHARACTER_TITLES.gnar = "The Missing Link"
+STRINGS.CHARACTER_NAMES.gnar = "Gnar"
+STRINGS.CHARACTER_DESCRIPTIONS.gnar = "*Time traveler\n*Has a boomerang\n*More balanced than League of Legends"
+STRINGS.CHARACTER_QUOTES.gnar = "\"Watch out for my Q move!\""
 
 -- Custom speech strings
-STRINGS.CHARACTERS.ESCTEMPLATE = require "speech_esctemplate"
+STRINGS.CHARACTERS.gnar = require "speech_gnar"
 
 -- The character's name as appears in-game 
-STRINGS.NAMES.ESCTEMPLATE = "Esc"
+STRINGS.NAMES.gnar = "Esc"
 
 -- Add mod character to mod character list. Also specify a gender. Possible genders are MALE, FEMALE, ROBOT, NEUTRAL, and PLURAL.
-AddModCharacter("esctemplate", "FEMALE")
+AddModCharacter("gnar", "MALE")
 
---dumb fix because Klei is dumb and changed the way bigportraits work without adjusting the API
---I hope this will be redundant in an update, but considering how long it's been since the issue came up, I doubt it. -M
--- Set this to use the correct (oval) bigportrait in the new character select screen. The old (square) bigportrait is still used for Inspect.
-GLOBAL.PREFAB_SKINS["esctemplate"] = {"esctemplate_none"}
-GLOBAL.PREFAB_SKINS_IDS["esctemplate"] = {["esctemplate_none"] = 1}
